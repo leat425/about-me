9d82617b4e62ce56afa77ab09d4f96847036d848
Anh Thu Le <leat425@gmail.com>
2018-06-07 23:05:40 -0700

    Updated all 7 questions with functions

--- js/app.js
+++ js/app.js
@@ -1,45 +1,43 @@
-// 'use strict';
+'use strict';
 
 var counter = 0;
 
-// // //Ask for user preferred name
+//Ask for user preferred name
 
-// // alert('This page will contain information about myself Before it displays, let\'s start with a silly game where you will answer with a Yes or No to a series of questions.  Are you ready to start the game?');
-// // var userName = prompt('Firstly, what is your preferred name?');
-// // console.log('Confirm user input for preferred name: '+ userName);
+alert('This page will contain information about myself Before it displays, let\'s start with a silly game where you will answer with a Yes or No to a series of questions.  Are you ready to start the game?');
+var userName = prompt('Firstly, what is your preferred name?');
+console.log('Confirm user input for preferred name: '+ userName);
 
-// var answerFalse = 'Please re-enter answer in Yes or No format';
-// var answerInCorrect = 'Incorrect guess';
-// var answerCorrect = 'Correct guess';
+var answerFalse = 'Please re-enter answer in Yes or No format';
+var answerInCorrect = 'Incorrect guess';
+var answerCorrect = 'Correct guess';
 
-// var questionOne = 'Do you think I have any coding experience?';
-// var questionTwo = 'I am very tall, do you think I am over 6 feet tall?';
-// var questionThree = 'Do I have any sisters?';
-// var questionFour = 'If I told you my dad is Bruce Lee, would you believe me?';
-// var questionFive = 'Do you think I can become a software developer in the future?';
+var questionOne = 'Do you think I have any coding experience?';
+var questionTwo = 'I am very tall, do you think I am over 6 feet tall?';
+var questionThree = 'Do I have any sisters?';
+var questionFour = 'If I told you my dad is Bruce Lee, would you believe me?';
+var questionFive = 'Do you think I can become a software developer in the future?';
 
-// function questionFunction(n) {
-//   var userInput = prompt(n).toLowerCase();
-//   console.log('User input: ' + userInput);
-//   if (userInput === 'no' || userInput === 'n') {
-//     alert(answerCorrect);
-//     counter++;
-//   } else if (userInput === 'yes' || userInput === 'y'){
-//     alert(answerInCorrect);
-//   } else {
-//     alert(answerFalse);
-//   }
-// }
-// questionFunction(questionOne);
-// questionFunction(questionTwo);
-// questionFunction(questionThree);
-// questionFunction(questionFour);
-// questionFunction(questionFive);
+function questionFunction(n) {
+  var userInput = prompt(n).toLowerCase();
+  console.log('User input: ' + userInput);
+  if (userInput === 'no' || userInput === 'n') {
+    alert(answerCorrect);
+    counter++;
+  } else if (userInput === 'yes' || userInput === 'y'){
+    alert(answerInCorrect);
+  } else {
+    alert(answerFalse);
+  }
+}
 
-// console.log('Number of questions guess correct: ' + counter);
+questionFunction(questionOne);
+questionFunction(questionTwo);
+questionFunction(questionThree);
+questionFunction(questionFour);
+questionFunction(questionFive);
 
 //Question 6: Please guess how many nieces I have?
-
 
 var tooHigh = 'Your guess is too high';
 var tooLow = 'Your guess is too low';
@@ -59,10 +57,9 @@ if (guessCorrected === false) {
 
 function guessNieces() {
   var userInput = parseInt(prompt('Please guess how many nices I have'));
-  console.log('User input: '+ userInput);
+  console.log('User input for number of nices : '+ userInput);
   if (userInput === 5) {
-    alert(answerCorrect);
-    counter++;
+    alert('Fantastic guess and it is correct!');
     return true;
 
   } else if (userInput > 5) {
@@ -74,69 +71,33 @@ function guessNieces() {
   return false;
 }
 
+//Question 7: Can you guess one of the 5 cities in the U.S. that I have visited?
 
+var cityCorrected = false;
 
+var visitedCity = ['san francisco', 'chicago', 'st.louis', 'las vegas', 'minnesota'];
 
-// var myNieces = 5;
+for (var i = 0; i < 7; i++) {
+  cityCorrected = guessCity();
+  if (cityCorrected === true) {
+    counter++;
+    break;
+  }
+}
 
-// for (var i = 0; i < 4 ; i++) {
-//   console.log('Number of try: ', i);
+if (cityCorrected === false) {
+  alert('Your guesses are close but the cities that I visited are San Francisco, Chicago, St.Louis, Las Vegas, Minnesota');
+}
 
-//   var guessNieces = parseInt(prompt('Please guess how many nieces I have?'));
-//   console.log('Confirm user input is: ' + guessNieces);
+function guessCity() {
+  var userInput = prompt('Can you guess one of the cities in the U.S. that I have visited?').toLowerCase();
+  console.log('User input City: ' + userInput);
+  if (userInput === visitedCity[i]) {
+    alert('That is correct!  Here are the cities that I visited: San Francisco, Chicago, St.Louis, Las Vegas, Minnesota');
+    return true;
+  }
+  return false;
+}
 
-//   if (guessNieces === myNieces) {
-//     alert('That is correct! I have 5 wonderful nieces');
-//     counter++;
-//     break;
-//   } else if (guessNieces > myNieces) {
-//     if (i === 3) {
-//       alert('That last guess was too high and also incorrect. That was the 4th attempt, maybe we should move on');
-//     } else {
-//       alert('Great guess but that number is too high, please guess again');
-//     }
-//   } else {
-//     if (i === 3) {
-//       alert('That last guess was too low and also incorrect. That was the 4th attempt, maybe we should move on');
-//     } else {
-//       alert('Great guess but that number is too low, please guess again.');
-//     }
-//   }
-// }
-
-// //Question 7: Can you guess one of the 5 cities in the U.S. that I have visited?
-
-// var visitedCity = ['san francisco', 'chicago', 'st.louis', 'las vegas', 'minnesota'];
-// var guessCorrected = false;
-
-// //first loop to limit user to 6 guesses
-// for(var j = 0; j < 6; j++) {
-//   var guessCity = prompt('Can you guess one of the cities in the U.S. that I have visited?').toLowerCase();
-//   console.log('Confirm user input: '+ guessCity);
-
-//   //second loop (nested inside) to check answer against array strings for correction. False will keep the user inside loop or until 6 tries.
-//   for (var i = 0; i < visitedCity.length; i++) {
-//     if (guessCity === visitedCity[i]) {
-//       guessCorrected = true;
-//       console.log('Confirm user guess one of the city correctly: '+ guessCity);
-//       break;
-//     }
-//   }
-//   //Guess corrected
-//   if(guessCorrected === true) {
-//     alert('Great job! You have guess corrected. The cities that I have visited are ' + visitedCity);
-//     counter++;
-//     break;
-//   }
-//   else {
-//     alert('Please try to guess again');
-//   }
-// }
-// //Incorrect guess
-// if(guessCorrected === false)
-// {
-//   alert('Your guesses are close but the cities that I visited are: '+ visitedCity);
-// }
-
-// //Last alert notify user the number of correct guesses
-// alert(userName + ', you have correctly answered ' + counter + ' out of 7 questions. Thank you for playing the game.');
+alert('You have correctly guessed ' + counter + ' out of 7.');
+console.log('Number of guesses that are corrected: '+ counter);
